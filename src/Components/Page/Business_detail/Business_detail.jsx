7c535d35e4876d26871a1324858d07d694400b3b
no_name <ahmedovyhlas001@gmail.com>
2024-01-05 13:47:32 +0300

    change_+__+_+

--- src/Components/Page/Business_detail/Business_detail.jsx
+++ src/Components/Page/Business_detail/Business_detail.jsx
@@ -28,7 +28,7 @@ const Business_detail = () => {
                 data ? <div class="news-details" >
                     <div class="container">
                         <div class="row">
-                            <div class="col-xl-8 col-lg-7">
+                            <div class="col-12">
                                 <div class="news-details__left">
                                     <h3 class="news-details__title mb-4">{data.title}
                                     </h3>
@@ -42,51 +42,6 @@ const Business_detail = () => {
                                         <p class="news-details__text-1">
                                             {data.description}
                                         </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-4 col-lg-5">
-                                <div class="sidebar">
-                                    <div class="sidebar__single sidebar__post">
-                                        <h3 class="sidebar__title">Latest posts</h3>
-                                        <ul class="sidebar__post-list list-unstyled">
-                                            <li>
-                                                <div class="sidebar__post-image">
-                                                    <img src={img_one} alt="" />
-                                                </div>
-                                                <div class="sidebar__post-content">
-                                                    <h3>
-                                                        <span class="sidebar__post-content-meta"><i
-                                                            class="fas fa-user-circle"></i>Admin</span>
-                                                        <a href="news-details.html">Flight's precautionary landing in dubai</a>
-                                                    </h3>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="sidebar__post-image">
-                                                    <img src={img_one} alt="" />
-                                                </div>
-                                                <div class="sidebar__post-content">
-                                                    <h3>
-                                                        <span class="sidebar__post-content-meta"><i
-                                                            class="fas fa-user-circle"></i>Admin</span>
-                                                        <a href="news-details.html">There are many variations of</a>
-                                                    </h3>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div class="sidebar__post-image">
-                                                    <img src={img_one} alt="" />
-                                                </div>
-                                                <div class="sidebar__post-content">
-                                                    <h3>
-                                                        <span class="sidebar__post-content-meta"><i
-                                                            class="fas fa-user-circle"></i>Admin</span>
-                                                        <a href="news-details.html">Bring to the table win-win survival</a>
-                                                    </h3>
-                                                </div>
-                                            </li>
-                                        </ul>
                                     </div>
                                 </div>
                             </div>
